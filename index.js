052c69c1e04927d2cab37e2cf9ed7b5da711e739
Pierre Bleeker <pierrebleeker@Pierres-MacBook-Pro.local>
2017-04-11 19:08:47 +0200

    make it better

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(3002, function () {
-// http.listen(process.env.PORT || 5000, function () {
+// http.listen(3002, function () {
+http.listen(process.env.PORT || 5000, function () {
     console.log('listening on *:3002');
 });

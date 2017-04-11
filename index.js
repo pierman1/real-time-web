de362d0bd94cbef0b4c99922191f5be48699eec4
Pierre Bleeker <pierrebleeker@Pierres-MacBook-Pro.local>
2017-04-11 18:46:12 +0200

    Made users available

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+
+var people = [];
+
+var index;
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+
+    console.log(data);
+    console.log(data);
+});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function (socket) {
     console.log('a user connected');
 
-    socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on("join", function(name){
+
+        // people[io.id] = name;
+        // io.emit("update", "You have connected to the server.");
+        // io.emit("update", name + " has joined the server.")
+        // io.emit("update-people", people);
+        //
+        // // console.log(name);
+        // // // console.log(people);
+        // // people.push(name);
+        // console.log(people);
+
+        console.log(name);
+        people.push(name);
+        console.log(people);
+
+
+        io.emit('join', people);
+    });
+
+    socket.on('chat message', function(msg, userName) {
+        // console.log('message: ' + msg);
+
+
+            io.emit('chat message', msg, userName);
+
     });
 
     socket.on('disconnect', function () {
@@ -20,6 +58,7 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(process.env.PORT || 5000, function () {
+http.listen(3002, function () {
+// http.listen(process.env.PORT || 5000, function () {
     console.log('listening on *:3002');
 });

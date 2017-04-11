37564bbdebb2751d1066b246581369c9dc535426
Pierre Bleeker <pierrebleeker@Pierres-MacBook-Pro.local>
2017-04-11 15:50:58 +0200

    Last commit on this branch

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('login');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
 });
 
 app.get('/chat', function (req, res) {
@@ -28,8 +32,28 @@ io.on('connection', function (socket) {
         io.emit('chat message', msg);
     });
 
+    socket.on('login', function(userName) {
+        console.log('user: ' + userName);
+        io.emit('login', userName);
+    });
+
     socket.on('disconnect', function () {
         console.log('user disconnected');
+    });
+
+    socket.emit('entrance', {message: 'Welcome to the chat room!'});
+    socket.emit('entrance', {message: 'Your ID is #' + socket.id});
+
+    socket.on('adduser', function (name) {
+        users.push(name);
+
+        // attempt to clean up
+        socket.once('disconnect', function () {
+            var pos = users.indexOf(name);
+
+            if (pos >= 0)
+                users.splice(pos, 1);
+        });
     });
 
 });
@@ -38,3 +62,5 @@ http.listen(3002, function () {
 // http.listen(process.env.PORT || 5000, function () {
     console.log('listening on *:3002');
 });
+
+var users = [];
